491b55b13fe47d4d20654a34305be95051eae879
Gungyphant <gungyphant@gmail.com>
2025-12-02 06:54:33 +0000

    Created working solution

--- Program.cs
+++ Program.cs
@@ -1,14 +1,44 @@
-﻿namespace AOC2025Day2
+﻿using System.Text.RegularExpressions;
+
+namespace AOC2025Day2
 {
     internal class Program
     {
         static string PartOne(string data)
         {
-            return "";
+            Regex regex = new Regex(@"^([0-9]+)\1$");
+            ulong total = 0;
+            foreach (string range in data.Split(','))
+            {
+                ulong start = ulong.Parse(range.Split("-")[0]);
+                ulong end = ulong.Parse(range.Split("-")[1]);
+                for (ulong i = start; i <= end; i++)
+                {
+                    if (regex.IsMatch(Convert.ToString(i)))
+                    {
+                        total += i;
+                    }
+                }
+            }
+            return total.ToString();
         }
         static string PartTwo(string data)
         {
-            return "";
+            Regex regex = new Regex(@"^([0-9]+)\1+$");
+            ulong total = 0;
+            foreach (string range in data.Split(','))
+            {
+                ulong start = ulong.Parse(range.Split("-")[0]);
+                ulong end = ulong.Parse(range.Split("-")[1]);
+                for (ulong i = start; i <= end; i++)
+                {
+                    if (regex.IsMatch(Convert.ToString(i)))
+                    {
+                        total += i;
+                    }
+                }
+            }
+            return total.ToString();
         }
         static void Main()
         {
